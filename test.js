1669948d1bb2015db41ffc68bd105e6eee2e8d6e
F1LT3R <al@pwn.io>
2018-04-12 23:40:41 -0400

    Better diff reporting

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const getPixels = require('get-pixels')
 const color = require('color')
 const chalk = require('chalk')
+const chromafi = require('chromafi')
 
 const loadPixels = path => new Promise((resolve, reject) => {
 	getPixels(path, function(err, pixels) {
@@ -43,9 +44,9 @@ const stat = (img, coords) => {
 	}
 
 	const average = color({
-		r: parseInt(stat.r, 10),
-		g: parseInt(stat.g, 10),
-		b: parseInt(stat.b, 10)
+		r: stat.r,
+		g: stat.g,
+		b: stat.b
 	}).hsl()
 
 	stat.hue = average.color[0]
@@ -92,6 +93,8 @@ const statGridSquares = (img, grid) =>
 		{s: stat(img, square.c)}
 	))
 
+
+
 const compare = async (img1, img2, opts) => {
 	const image1 = await loadPixels(img1)
 	const image2 = await loadPixels(img2)
@@ -108,140 +111,195 @@ const compare = async (img1, img2, opts) => {
 	const diffLum = Array(size).fill(0)
 	const diffAll = Array(size).fill(0)
 	const diffClr = Array(size).fill(0)
+	const diffAlp = Array(size).fill(0)
 
 	const gridStat1 = statGridSquares(image1, grid1)
 	const gridStat2 = statGridSquares(image2, grid2)
+
+	let maxHueDiff = 0
+	let maxSatDiff = 0
+	let maxLumDiff = 0
+	let maxAlpDiff = 0
+
+	const grad = ' ░▒▓█'
 
 	gridStat1.forEach((square, i) => {
 		const hueDiff = square.s.hue - gridStat2[i].s.hue
 		if (hueDiff > opts.tolerance.hue) {
 			diffHue[i] += 1
 			diffAll[i] += 1
+
+			if (hueDiff > maxHueDiff) {
+				maxHueDiff = hueDiff
+			}
 		}
 
 		const satDiff = square.s.sat - gridStat2[i].s.sat
 		if (satDiff > opts.tolerance.sat) {
 			diffSat[i] += 1
 			diffAll[i] += 1
+
+			if (satDiff > maxSatDiff) {
+				maxSatDiff = satDiff
+			}
 		}
 
 		const lumDiff = square.s.lum - gridStat2[i].s.lum
-		if (satDiff > opts.tolerance.lum) {
+		if (lumDiff > opts.tolerance.lum) {
 			diffLum[i] += 1
 			diffAll[i] += 1
+
+			if (lumDiff > maxLumDiff) {
+				maxLumDiff = lumDiff
+			}
 		}
 
-		const diffCol = color({
-			h: parseInt(hueDiff),
-			s: parseInt(satDiff),
-			l: parseInt(lumDiff),
-		}).rgb().color
-		diffClr[i] = chalk.rgb(...diffCol)
+		const alpDiff = square.s.a - gridStat2[i].s.a
+		if (alpDiff > opts.tolerance.alpha) {
+			diffAlp[i] += 1
+			diffAll[i] += 1
+
+			if (alpDiff > maxAlpDiff) {
+				maxalpDiff = alpDiff
+			}
+		}
+
+		const h = parseInt((360 / maxHueDiff) * hueDiff || 1)
+		const s = parseInt((100 / maxSatDiff) * satDiff || 1)
+		const l = parseInt((100 / maxLumDiff) * lumDiff || 1)
+
+		const diffCol = color({h, s, l})
+			.rgb().color.map(c => parseInt(c))
+
+		diffClr[i] = {
+			col: chalk.bgRgb(...diffCol).rgb(...diffCol),
+			char: grad[parseInt(((grad.length-1) / 100) * l)]
+		}
 	})
 
-	// while (diffHue.length > 0) {
-	// 	const row = diffHue.splice(0, opts.grid.columns)
-	// 	row.forEach(d => {
-	// 		if (d) {
-	// 			process.stdout.write(chalk.redBright('█'))
-	// 		} else {
-	// 			process.stdout.write(chalk.green('░'))
-	// 		}
-	// 	})
-	// 	process.stdout.write('\n')
-	// }
-
-	// console.log()
-	// while (diffSat.length > 0) {
-	// 	const row = diffSat.splice(0, opts.grid.columns)
-	// 	row.forEach(d => {
-	// 		if (d) {
-	// 			process.stdout.write(chalk.redBright('█'))
-	// 		} else {
-	// 			process.stdout.write(chalk.green('░'))
-	// 		}
-	// 	})
-	// 	process.stdout.write('\n')
-	// }
-
-	// console.log()
-	// while (diffLum.length > 0) {
-	// 	const row = diffLum.splice(0, opts.grid.columns)
-	// 	row.forEach(d => {
-	// 		if (d) {
-	// 			process.stdout.write(chalk.redBright('█'))
-	// 		} else {
-	// 			process.stdout.write(chalk.green('░'))
-	// 		}
-	// 	})
-	// 	process.stdout.write('\n')
-	// }
-
-	console.log()
-	while (diffAll.length > 0) {
-		const row = diffAll.splice(0, opts.grid.columns)
-		row.forEach(d => {
-			if (d) {
-				process.stdout.write(chalk.redBright('█'))
-			} else {
-				process.stdout.write(chalk.green('░'))
-			}
-		})
-		process.stdout.write('\n')
+	const bMap = {
+		H___: '▘',
+		_S__: '▝',
+		__L_: '▖',
+		___A: '▗',
+		HS__: '▀',
+		HSL_: '▛',
+		HSLA: '█',
+		_SL_: '▞',
+		_SLA: '▟',
+		__LA: '▄',
+		H__A: '▚',
+		H_L_: '▌',
+		_S_A: '▐',
+		H_LA: '▙',
+		HS_A: '▜',
 	}
 
+
 	console.log()
+	let flagOutput = ''
+	for (let y = 0; y < opts.grid.rows; y++) {
+		for (let x = 0; x < opts.grid.columns; x++) {
+			const n = y * (opts.grid.columns) + x
+			let b = ''
+			b += diffHue[n] ? 'H' : '_'
+			b += diffSat[n] ? 'S' : '_'
+			b += diffLum[n] ? 'L' : '_'
+			b += diffAlp[n] ? 'A' : '_'
+			const bChar = bMap[b] || ''
+
+			if (bChar) {
+				flagOutput += chalk.bgRedBright.bold.black(bChar)
+			} else {
+				flagOutput += chalk.bgGreen(' ')
+			}
+		}
+		flagOutput += '\n'
+	}
+	console.log(flagOutput)
+
+	const toleranceDiff = {
+		hue: maxHueDiff,
+		sat: maxSatDiff,
+		lum: maxLumDiff,
+		alp: maxAlpDiff
+	}
+
+	console.log(chalk.magenta('Actual difference in tolerance:'))
+	console.log(chalk.italic.gray('(Use these values in opts.tolerance to pass the test.)'))
+	console.log(chromafi(toleranceDiff, {
+		lineNumbers: false,
+		codePad: 0
+	}))
+
+	console.log()
+	let diffOutput = ''
 	while (diffClr.length > 0) {
 		const row = diffClr.splice(0, opts.grid.columns)
 		row.forEach(d => {
-			process.stdout.write(d('█'))
+			diffOutput += d.col(d.char)
 		})
-		process.stdout.write('\n')
+		diffOutput += '\n'
 	}
+	console.log(diffOutput)
 
-	const sz = 8
-	for (let y = 0; y < image1.shape[1]; y += sz) {
-		for (let x = 0; x < image1.shape[0]; x += sz) {
-			const idx = ((image1.shape[0] * y) + x) << 2
-			if (idx + 4 <= image1.data.length) {
-				const r = image1.data[idx]
-				const g = image1.data[idx + 1]
-				const b = image1.data[idx + 2]
-				const a = image1.data[idx + 3]
-				const d = chalk.rgb(r, g, b)
-				process.stdout.write(d('█'))
+	if (opts.showExpected) {
+		console.log()
+		console.log('Expected')
+		let output = ''
+		const sz = 1 / process.stdout.columns * image1.shape[0]
+		for (let y = 0; y < image1.shape[1]; y += sz) {
+			for (let x = 0; x < image1.shape[0]; x += sz) {
+				const idx = ((image1.shape[0] * parseInt(y)) + parseInt(x)) << 2
+				if (idx + 4 <= image1.data.length) {
+					const r = image1.data[idx]
+					const g = image1.data[idx + 1]
+					const b = image1.data[idx + 2]
+					const d = chalk.bgRgb(r, g, b)
+					output += d(' ')
+				}
 			}
 		}
-		process.stdout.write('\n')
+		console.log(output)
 	}
 
-
-
-	// diff.forEach((mark, i) => {
-	// 	if (mark > 0) {
-
-	// 	}
-
-	// })
-
-	// console.log(gridStat1.map(n => [
-	// 	n.s.hue.toFixed(0).padStart(3),
-	// 	n.s.lum.toFixed(0).padStart(3),
-	// 	n.s.sat.toFixed(0).padStart(3),
-	// ]))
-	// console.log(gridStat2.map(n => [
-	// 	n.s.hue.toFixed(0).padStart(3),
-	// 	n.s.lum.toFixed(0).padStart(3),
-	// 	n.s.sat.toFixed(0).padStart(3),
-	// ]))
-
+	if (opts.showActual) {
+		console.log()
+		console.log('Actual')
+		let output2 = ''
+		const sz2 = 1 / process.stdout.columns * image2.shape[0]
+		for (let y = 0; y < image2.shape[1]; y += sz2) {
+			for (let x = 0; x < image2.shape[0]; x += sz2) {
+				const idx = ((image2.shape[0] * parseInt(y)) + parseInt(x)) << 2
+				if (idx + 4 <= image2.data.length) {
+					const r = image2.data[idx]
+					const g = image2.data[idx + 1]
+					const b = image2.data[idx + 2]
+					const d = chalk.bgRgb(r, g, b)
+					output2 += d(' ')
+				}
+			}
+		}
+		console.log(output2)
+	}
 }
 
-const expected = './fixtures/local-wish-command.png'
-const actual = './fixtures/server-wish-command.png'
+// THE SAME FILE
+// const expected = './fixtures/local-wish-command.png'
+// const actual = './fixtures/local-wish-command.png'
 
+// // THE SAME GRAPHIC RENDERED ON 2 DIFFERENT MACHINES
+// const expected = './fixtures/local-wish-command.png'
+// const actual = './fixtures/server-wish-command.png'
+
+// // THE SAME GRAPHIC AT DIFFERENT SIZES
 // const expected = './fixtures/local-wish-command-sml.png'
 // const actual = './fixtures/local-wish-command.png'
+
+// VERY DIFFERENT IMAGES
+const expected = './fixtures/emojis.png'
+const actual = './fixtures/iterm2colors-file.png'
+
 
 // const expected = './fixtures/server-wish-command.png'
 // const actual = './fixtures/iterm2colors-file.png'
@@ -249,8 +307,6 @@ const actual = './fixtures/server-wish-command.png'
 // const expected = './fixtures/local-wish-command.png'
 // const actual = './fixtures/iterm2colors-file.png'
 
-// const expected = './fixtures/emojis.png'
-// const actual = './fixtures/iterm2colors-file.png'
 // const actual = './fixtures/emojis.png'
 
 // const expected = './fixtures/IMG_20180401_134432.jpg'
@@ -258,13 +314,14 @@ const actual = './fixtures/server-wish-command.png'
 
 const opts = {
 	grid: {
-		columns: 16,
-		rows: 8
+		columns: 32,
+		rows: 16
 	},
 	tolerance: {
-		hue: 5,
-		sat: 30,
-		lum: 1000
+	    hue: 0,
+	    sat: 0,
+	    lum: 0,
+	    alp: 0
 	}
 }
 
